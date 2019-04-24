f74c0bf4d62462cd720ecbfc9f15777e56b57b22
Andrew Martinez <andrew.j.martinez@student.ie.edu>
2019-04-24 09:40:44 +0200

    Setup outline of core functionality needed

--- src/ie_group_1_mini_pandas/__init__.py
+++ src/ie_group_1_mini_pandas/__init__.py
@@ -1,8 +1,69 @@
-def sum_numbers(a,b):
-    return a + b
+"""
+Mini_Pandas DataFrame
+---------
+Customized version of a DataFrame meant to mimic core functionalities of the Pandas DataFrame object.
 
-def tokenize(sentence, lower = False):
-    if lower:
-        return sentence.lower().split()
-    else:
-        return sentence.split()
+"""
+
+import numpy as np
+import pandas as pd
+
+class MiniDataFrame:
+    def __init__(self, data = None, index = None, columns = None, datatype = None): 
+        if data is None:
+            data = {}
+    
+    @property
+    def get_row(self):
+        """
+        Return a list representing the corresponding values of the given row.
+        Examples
+        --------
+        >>> df = minipd.MiniDataFrame({'col1': [1, 2], 'col2': [3, 4]})
+        >>> df.getrow
+
+        """
+
+    @property
+    def sum(self):
+         """
+        Return a list of values corresponding to the sum of each numerical column while ignoring non-numerical columns.
+        Examples
+        --------
+        >>> df = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
+        >>> df.sum
+
+        """
+
+    @property
+    def median(self):
+        """
+        Return a list of values corresponding to the median of each numerical column while ignoring non-numerical columns.
+        Examples
+        --------
+        >>> df = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
+        >>> df.median
+
+        """
+
+    @property
+    def min(self):
+        """
+        Return a list of values corresponding to the min of each numerical column while ignoring non-numerical columns.
+        Examples
+        --------
+        >>> df = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
+        >>> df.min
+
+        """
+    
+    @property
+    def max(self):
+        """
+        Return a list of values corresponding to the max of each numerical column while ignoring non-numerical columns.
+        Examples
+        --------
+        >>> df = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
+        >>> df.max
+
+        """
