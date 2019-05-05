1939b574f557d8fb1cfb37c10c9d169ffb9c4a2f
Andrew Martinez <andrew.j.martinez@student.ie.edu>
2019-05-05 17:14:32 +0200

    Adding PYTEST param to shape and get_row

--- tests/get_row_test.py
+++ tests/get_row_test.py
@@ -3,35 +3,28 @@ import numpy as np
 import ie_group_1_mini_pandas as mini_pd
 
 
-def test_get_row():
-    dictionary1 = {'col1': [1, 2], 'col2': [3, 4]}
-    mdf1 = mini_pd.MiniDataFrame(dictionary1)
-    mdf_row1 = mdf1.get_row(1)
-    assert mdf_row1[0] == 2
-    assert mdf_row1[1] == 4
+@pytest.mark.parametrize('dic, expected_result',
+                         [
+                             ({'col1': [1, 2], 'col2': [3, 4]}, [2, 4]),
+                             ({'col1': [1, 2, 3], 'col2': [4, 5, 6]}, [2, 5]),
+                             ({'col1': [1, 2], 'col2': [3, 4], 'col3': [5, 6]},
+                              [2, 4, 6])
+                         ]
+                         )
+def test_get_row_dict(dic, expected_result):
+    dictionary = dic
+    df = mini_pd.MiniDataFrame(dictionary)
+    assert all([a == b for a, b in zip(df.get_row(1), expected_result)])
 
-    dictionary2 = {'col1': [1, 2, 3], 'col2': [4, 5, 6]}
-    mdf2 = mini_pd.MiniDataFrame(dictionary2)
-    mdf_row2 = mdf2.get_row(1)
-    assert mdf_row2[0] == 2
-    assert mdf_row2[1] == 5
 
-    dictionary3 = {'col1': [1, 2], 'col2': [3, 4], 'col3': [5, 6]}
-    mdf3 = mini_pd.MiniDataFrame(dictionary3)
-    mdf_row3 = mdf3.get_row(1)
-    assert mdf_row3[0] == 2
-    assert mdf_row3[1] == 4
-    assert mdf_row3[2] == 6
-
-    array1 = mini_pd.MiniDataFrame(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
-                                   columns=['a', 'b', 'c'])
-    array_row1 = array1.get_row(1)
-    assert array_row1[0] == 4
-    assert array_row1[1] == 5
-    assert array_row1[2] == 6
-
-    array2 = mini_pd.MiniDataFrame(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
-    array_row2 = array2.get_row(1)
-    assert array_row2[0] == 4
-    assert array_row2[1] == 5
-    assert array_row2[2] == 6
+@pytest.mark.parametrize('array, cols, expected_result',
+                         [
+                            (np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
+                             ['a', 'b', 'c'], [4, 5, 6]),
+                            (np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), None,
+                             [4, 5, 6])
+                         ]
+                         )
+def test_get_row_array(array, cols, expected_result):
+    df = mini_pd.MiniDataFrame(array, columns=cols)
+    assert all([a == b for a, b in zip(df.get_row(1), expected_result)])
--- tests/shape_test.py
+++ tests/shape_test.py
@@ -4,12 +4,13 @@ import ie_group_1_mini_pandas as mini_pd
 
 
 @pytest.mark.parametrize('dic, expected_result',
-    [
-        ({'col1': [1, 2], 'col2': [3, 4]}, (2, 2)),
-        ({'col1': [1, 2, 3], 'col2': [4, 5, 6]}, (3, 2)),
-        ({'col1': [1, 2], 'col2': [3, 4], 'col3': [5, 6]}, (2, 3))
-    ]
-)
+                         [
+                            ({'col1': [1, 2], 'col2': [3, 4]}, (2, 2)),
+                            ({'col1': [1, 2, 3], 'col2': [4, 5, 6]}, (3, 2)),
+                            ({'col1': [1, 2], 'col2': [3, 4], 'col3': [5, 6]},
+                             (2, 3))
+                         ]
+                         )
 def test_shape_dict(dic, expected_result):
     dictionary = dic
     df = mini_pd.MiniDataFrame(dictionary)
@@ -17,11 +18,13 @@ def test_shape_dict(dic, expected_result):
 
 
 @pytest.mark.parametrize('array, cols, expected_result',
-    [
-        (np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), ['a', 'b', 'c'], (3, 3)),
-        (np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), None, (3, 3))
-    ]
-)
+                         [
+                            (np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
+                             ['a', 'b', 'c'], (3, 3)),
+                            (np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), None,
+                             (3, 3))
+                         ]
+                         )
 def test_shape_array(array, cols, expected_result):
     df = mini_pd.MiniDataFrame(array, columns=cols)
     assert df.shape == expected_result
