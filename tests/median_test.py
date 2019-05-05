05af4584815a67d998b1e795fa90ad178d03a5fb
Andrew Martinez <andrew.j.martinez@student.ie.edu>
2019-05-05 13:52:07 +0200

    PEP 8 compliant

--- tests/median_test.py
+++ tests/median_test.py
@@ -1,9 +1,10 @@
 import pytest
 import ie_group_1_mini_pandas as mini_pd
 
+
 def test_median():
-    dictionary = {'col1': [1,2,3], 'col2': [4,5,6]}
+    dictionary = {'col1': [1, 2, 3], 'col2': [4, 5, 6]}
     mdf = mini_pd.MiniDataFrame(dictionary)
     median_mdf = mdf.median
-    assert median_mdf[0] == 2 
-    assert median_mdf[1] == 5 
+    assert median_mdf[0] == 2
+    assert median_mdf[1] == 5
--- tests/sum_test.py
+++ tests/sum_test.py
@@ -1,9 +1,10 @@
 import pytest
 import ie_group_1_mini_pandas as mini_pd
 
+
 def test_sum():
-    dictionary = {'col1': [1,2], 'col2': [3,4]}
+    dictionary = {'col1': [1, 2], 'col2': [3, 4]}
     mdf = mini_pd.MiniDataFrame(dictionary)
     sum_mdf = mdf.sum
-    assert sum_mdf[0] == 3 # col1: 1 + 2
-    assert sum_mdf[1] == 7 # col2: 3 + 4
+    assert sum_mdf[0] == 3  # col1: 1 + 2
+    assert sum_mdf[1] == 7  # col2: 3 + 4
