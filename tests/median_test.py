cb196eb91440d53a94495a2b67104b93a1d19af6
Andrew Martinez <andrew.j.martinez@student.ie.edu>
2019-05-05 18:24:10 +0200

    PEP 8 Compliant

--- tests/median_test.py
+++ tests/median_test.py
@@ -3,16 +3,19 @@ import numpy as np
 import ie_group_1_mini_pandas as mini_pd
 
 
-@pytest.mark.parametrize('dic_array, cols, expected_result', 
-    [
-        ({'col1': [2, 4], 'col2': [4, 8]}, None, [3, 6]),
-        ({'col1': [1, 2, 3], 'col2': [4, 5, 6]}, None, [2, 5]),
-        ({'col1': [2, 4], 'col2': [6, 8], 'col3': [10, 12]}, None, [3, 7, 11]),
-        (np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), ['a', 'b', 'c'], [4, 5, 6]),
-        (np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), None, [4, 5, 6])
-    ]
-)
-
+@pytest.mark.parametrize('dic_array, cols, expected_result',
+                         [
+                            ({'col1': [2, 4], 'col2': [4, 8]}, None, [3, 6]),
+                            ({'col1': [1, 2, 3], 'col2': [4, 5, 6]}, None,
+                             [2, 5]),
+                            ({'col1': [2, 4], 'col2': [6, 8],
+                             'col3': [10, 12]}, None, [3, 7, 11]),
+                            (np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
+                             ['a', 'b', 'c'], [4, 5, 6]),
+                            (np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
+                             None, [4, 5, 6])
+                         ]
+                         )
 def test_median(dic_array, cols, expected_result):
     df = mini_pd.MiniDataFrame(dic_array, columns=cols)
     median_df = df.median
--- tests/min_test.py
+++ tests/min_test.py
@@ -3,16 +3,20 @@ import numpy as np
 import ie_group_1_mini_pandas as mini_pd
 
 
-@pytest.mark.parametrize('dic_array, cols, expected_result', 
-    [
-        ({'col1': [2, 4], 'col2': [4, 8]}, None, [2, 4]),
-        ({'col1': [1, 2, 3], 'col2': [4, 5, 6]}, None, [1, 4]),
-        ({'col1': [2, 4], 'col2': [6, 8], 'col3': [10, 12]}, None, [2, 6, 10]),
-        (np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), ['a', 'b', 'c'], [1, 2, 3]),
-        (np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]), None, [1, 2, 3])
-    ]
-)
-
+@pytest.mark.parametrize('dic_array, cols, expected_result',
+                         [
+                            ({'col1': [2, 4], 'col2': [4, 8]},
+                             None, [2, 4]),
+                            ({'col1': [1, 2, 3], 'col2': [4, 5, 6]},
+                             None, [1, 4]),
+                            ({'col1': [2, 4], 'col2': [6, 8],
+                             'col3': [10, 12]}, None, [2, 6, 10]),
+                            (np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
+                             ['a', 'b', 'c'], [1, 2, 3]),
+                            (np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
+                             None, [1, 2, 3])
+                         ]
+                         )
 def test_min(dic_array, cols, expected_result):
     df = mini_pd.MiniDataFrame(dic_array, columns=cols)
     min_df = df.min
