57e1af86af8b9f4fc8c915fd93ea5cadffc8eb8e
Duarte Afonso <duarte.afonso@student.ie.edu>
2019-05-05 03:02:13 +0200

    corrected minor mistake in shape test

--- tests/shape_test.py
+++ tests/shape_test.py
@@ -1,7 +1,7 @@
 import pytest
 import ie_group_1_mini_pandas as mini_pd
 
-def shape_test():
+def test_shape():
     dictionary = {'col1': [1, 2], 'col2': [3, 4]}
     df = mini_pd.MiniDataFrame(dictionary)
     assert df.shape == (2,2)
